4dac3906f5b665dedacb0c125ae424d3a80d81fb
Jakub Biały <jakub@klavenessdigital.com>
2022-09-10 19:39:41 +0200

    some options for happy number

--- src/main/scala/leetCode75/level2/HappyNumber.scala
+++ src/main/scala/leetCode75/level2/HappyNumber.scala
@@ -8,13 +8,12 @@ object HappyNumber extends App {
   def isHappyTailrecWithCycleDetection(n: Int): Boolean = {
     @tailrec
     def cycle(n: Int, loop: Int = 0, set: Set[Int]): Boolean = {
-      if (loop == 200 ||
-        set.contains(n)
-      ) return false
+      if (loop == 200 || set.contains(n)) return false
       else if (n == 1) return true
 
       val newSet = set + n
-      val numbers = n.toString.map(_.asDigit)
+      val numbers = toSingleDigitsAlt(n)
+      println(numbers)
       val squarePowNumbers = numbers.map(Math.pow(_, 2).toInt).sum
       cycle(squarePowNumbers, loop + 1, newSet)
     }
@@ -28,7 +27,7 @@ object HappyNumber extends App {
       if (loop == 200) return false
       else if (n == 1) return true
 
-      val numbers = n.toString.map(_.asDigit)
+      val numbers = toSingleDigitsAlt(n)
       val squarePowNumbers = numbers.map(Math.pow(_, 2).toInt).sum
       cycle(squarePowNumbers, loop + 1)
     }
@@ -36,5 +35,19 @@ object HappyNumber extends App {
     cycle(n)
   }
 
+  private def toSingleDigits(n: Int) = {
+    n.toString.map(_.asDigit)
+  }
+
+  @tailrec
+  private def toSingleDigitsAlt(n: Int, acc: List[Int] = List.empty): List[Int] = {
+    if (n == 0) return acc
+    val number = n % 10
+    val newNumber = n / 10
+    toSingleDigitsAlt(newNumber, number :: acc)
+  }
+
+
   println(isHappyTailrecWithCycleDetection(116))
+  println(isHappyTailrecWithCycleDetection(82))
 }
