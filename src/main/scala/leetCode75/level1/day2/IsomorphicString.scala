d6d676b06f959e50694d72f9e22855c8fc74ae9e
Jakub Biały <jakub@klavenessdigital.com>
2022-09-12 10:39:47 +0200

    Alternative Isomorphic string solution

--- src/main/scala/leetCode75/level1/day2/IsomorphicString.scala
+++ src/main/scala/leetCode75/level1/day2/IsomorphicString.scala
@@ -1,5 +1,7 @@
 package leetCode75.level1.day2
 
+import scala.annotation.tailrec
+
 object IsomorphicString extends App {
 
   def isIsomorphic(s: String, t: String): Boolean = {
@@ -8,9 +10,8 @@ object IsomorphicString extends App {
       val key = y._2
 
       val areValuesSameForSameKeys = if (acc.contains(key)) acc.get(key).contains(value) else true
-      //      different key can't have already existing value
-      val getOtherValues = acc.keys.filterNot(_ == key).toSet.flatMap(acc.get)
-      val noOtherKeysContainsSameV = getOtherValues.toList.contains(value)
+      val valuesForOtherKeys = (acc.keys.toSet - key).flatMap(acc.get)
+      val noOtherKeysContainsSameV = valuesForOtherKeys.toList.contains(value)
 
       if (!areValuesSameForSameKeys || noOtherKeysContainsSameV) return false
 
@@ -19,17 +20,39 @@ object IsomorphicString extends App {
     true
   }
 
+  //  seems to be slightly faster, and much less memory consuming (80%!)
+  def isIsomorphicV2(s: String, t: String): Boolean = {
+    @tailrec
+    def isIsomorphicTailRec(zipped: List[(Char, Char)], acc: Map[Char, Char] = Map.empty): Boolean = {
+      val (key, value) = zipped match {
+        case head :: _ => head
+        case _ => return true
+      }
 
-  val s = "egg"
-  val t = "add"
-  //  val s = "foo"
-  //  val t = "bar"
-  //      val s = "paper"
-  //      val t = "title"
-  //        val s = "bbbaaaba"
-  //        val t = "aaabbbba"
-  //  val s = "badc"
-  //  val t = "baba"
+      val areValuesSameForSameKeys = if (acc.contains(key)) acc.get(key).contains(value) else true
+      val valuesForOtherKeys = (acc.keys.toSet - key).flatMap(acc.get)
+      val noOtherKeysContainsSameV = valuesForOtherKeys.toList.contains(value)
+
+      if (!areValuesSameForSameKeys || noOtherKeysContainsSameV) return false
+
+      isIsomorphicTailRec(zipped.drop(1), acc + (key -> value))
+    }
+
+    isIsomorphicTailRec(s.toCharArray.zip(t.toCharArray).toList)
+  }
+
+
+  //    val s = "egg"
+  //    val t = "add"
+  //      val s = "foo"
+  //      val t = "bar"
+  //  val s = "paper"
+  //  val t = "title"
+  //  val s = "bbbaaaba"
+  //  val t = "aaabbbba"
+  val s = "badc"
+  val t = "baba"
 
   println(isIsomorphic(s, t))
+  println(isIsomorphicV2(s, t))
 }
